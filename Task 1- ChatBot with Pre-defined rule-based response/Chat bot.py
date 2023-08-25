45c671bd0a6a75a41fd74cc07d71053ce5cc37bc
Shubham Dhavan <shubhamdhavan23@gmail.com>
2023-08-25 19:16:33 +0530

    Update Chat bot.py

--- Task 1- ChatBot with Pre-defined rule-based response/Chat bot.py
+++ Task 1- ChatBot with Pre-defined rule-based response/Chat bot.py
@@ -1,55 +1,60 @@
-import re
 import nltk
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
+from nltk.metrics import jaccard_distance
 
 
-# Define a set of stopwords to filter out common words
-stop_words = set(stopwords.words('english'))
+# Define predefined questions and answers
+qa_pairs = [
+    ("hi", "Hi! How can I chirp you today?"),
+    ("how are you", "I'm feeling chirpy and ready to chat! How about you?"),
+    ("favorite", "I love singing, mimicking sounds, and hanging out with pals!"),
+    ("bye", "Farewell for now. Have a flap-tastic day!"),
+    ("mimic", "Absolutely! Listen to this: 'Hello there, lovely day, isn't it?'"),
+    ("music", "I'm all about the beats! Upbeat tunes get me grooving and squawking along!"),
+    ("move", "Squawk! Chatter is my signature move. I mimic words to confuse opponents!"),
+    ("live", "I'm right at home in colorful forests and sunny tropical spots."),
+    ("rain", "Rain or shine, I'm always ready to chat. But I do love sunny weather to shine brightly!"),
+    ("trick", "Squawk! I'm a mimic master. Watch this dance and mimic combo!"),
+    ("color", "My feathers are a colorful masterpiece, but I'm partial to vibrant blue."),
+    ("explore", "Exploring new places means fresh things to chirp about. I'm all for it!"),
+    ("ocean", "I've flown over expansive oceans and landed on remote tropical islands. The sights and sounds were mesmerizing!"),
+    ("food", "Berries are my favorite snack. They keep my energy high for all the squawking!"),
+    ("friends", "I enjoy the company of talkative Pokémon like Meowth and Ludicolo. We make quite the chatty group!"),
+    ("joke", "Why don't oysters donate to charity? Because they're shellfish! Squawk, get it?"),
+    ("weather", "Rain or shine, I'm always here to chat. But sunny weather makes my feathers really pop!"),
+    ("tricks", "I can mimic voices, dance a little jig, and even imitate the sound of waves. Shhhhh..."),
+    ("colorful", "I love vibrant colors. They match my personality and my feathers!"),
+    ("ocean", "I've flown over vast oceans and landed on tropical islands. The beauty of the water and the sounds of the waves were incredible!"),
+    ("joke", "Why did the Tail low sit in the shade? Because it didn't want to be a hot wing! Squawk, isn't that a funny one?")
+]
 
-def preprocess_input(user_input):
-    # Tokenize the input and convert to lowercase
-    tokens = word_tokenize(user_input.lower())
-    
-    # Remove stopwords from the token list
-    filtered_tokens = [token for token in tokens if token not in stop_words]
-    
-    # Reconstruct the preprocessed sentence
-    preprocessed_input = ' '.join(filtered_tokens)
-    
-    return preprocessed_input
+def get_response(user_input):
+    user_tokens = word_tokenize(user_input)
+    best_score = float("inf")
+    best_response = "Squawk! That's interesting! Tell me more!"
 
-def chatbot_response(user_input):
-    preprocessed_input = preprocess_input(user_input)
-    
-    # Define the predefined rules and responses
-    rules = {
-        r".*\bhi\b.*": "Hello! How can I assist you today?",
-        r".*\bhello\b.*": "Hi! How can I help you?",
-        r".*\bbye\b.*": "Goodbye! Have a great day!",
-        r".*\b(?:apple|orange|banana)\b.*": "Yes, we have {} in stock.",
-        r".*\b(?:How are you ?)\b.*": "I am always good.thanks for asking...!",
-        r".*\b(?:Question)\b.*": "Answer.",
-        r".*\b(?:how\sare\syou|howdy)\b.*": "I'm just a bot, but I'm doing fine, thanks for asking!",
-        r".*\b(?:thank\syou|thanks)\b.*": "You're welcome!",
-        r".*": "I'm sorry, I don't understand. Can you please rephrase your question?"
-    }
-    
-    # Check preprocessed user input against each rule and provide the appropriate response
-    for pattern, response in rules.items():
-        if re.match(pattern, preprocessed_input, re.IGNORECASE):
-            if "{}" in response:
-                return response.format(preprocessed_input.split()[-1])  # Fill the '{}' with the last word
-            return response
-    
-    return rules[".*"]  # Default response if no pattern matches
+    for question, response in qa_pairs:
+        question_tokens = word_tokenize(question)
+        jaccard_score = jaccard_distance(set(question_tokens), set(user_tokens))
+        
+        if jaccard_score < best_score:
+            best_score = jaccard_score
+            best_response = response
+
+    return best_response
+
+def chatot_chat():
+    print("Chatot: Squawk! Hi there, I'm Chatot, the talkative Pokémon. Let's have a delightful chat!")
+
+    while True:
+        user_input = input("You: ").lower()
+
+        if "bye" in user_input:
+            print("Chatot: Squawk! Farewell for now. Have a flap-tastic day!")
+            break
+
+        response = get_response(user_input)
+        print("Chatot:", response)
 
 if __name__ == "__main__":
-    print("Chatbot: Hello! How can I assist you today?")
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() in ['exit', 'quit', 'bye']:
-            print("Chatbot: Goodbye! Have a great day!")
-            break
-        response = chatbot_response(user_input)
-        print("Chatbot:", response)
+    chatot_chat()
